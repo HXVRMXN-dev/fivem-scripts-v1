ae2388f91394ab2f6cd695e44a0eb9295d8fd185
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2022-08-21 18:34:59 +0100

    Update server.lua

--- HXVRMXN-Zipties/server.lua
+++ HXVRMXN-Zipties/server.lua
@@ -15,16 +15,15 @@ end)
 
 
 -- Commands 
-
 RegisterCommand("zip", function(source, args, message)
-    if(config.enable_perms == true) then
-        if IsPlayerAceAllowed(source, config.ace_perm) then
-                TriggerClientEvent("ClientTie", source)
-        end 
-        else 
-            TriggerClientEvent('chatMessage', source, "^1Insufficent Permissions")
-        end
-    end
+     if(config.enable_perms == true) then
+         if IsPlayerAceAllowed(source, config.ace_perm) then
+                 TriggerClientEvent("ClientTie", source)
+         end 
+         else 
+             TriggerClientEvent('chatMessage', source, "^1Insufficent Permissions")
+         end
+      end
 end)
 
 
