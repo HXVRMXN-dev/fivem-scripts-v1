5c96510314a9334a2630d9345591709e2f2bc2e2
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2022-08-24 02:58:08 +0100

    Update server.lua

--- HXVRMXN-Bodycam/server.lua
+++ HXVRMXN-Bodycam/server.lua
@@ -1,1 +1,15 @@
+resourcename = "HXVRMXN's Bodycams"
+version = "1.0.1"
+rawVersionLink = "https://pastebin.com/raw/AC0RTgrb"
 
+
+-- Check for version updates.
+if versionChecker then
+PerformHttpRequest(rawVersionLink, function(errorCode, result, headers)
+    if (string.find(tostring(result), version) == nil) then
+        print("\n\r[".. GetCurrentResourceName() .."] ^1WARNING: Your version of ".. resourcename .." is not up to date. Please make sure to update whenever possible.\n\r")
+    else
+        print("\n\r[".. GetCurrentResourceName() .."] ^2You are running the latest version of ".. resourcename ..".\n\r")
+    end
+end, "GET", "", "")
+end
